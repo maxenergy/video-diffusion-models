072a249c3878661986fe1301943110fefb6f2edc
Vallasc <giacomovallorani@protonmail.com>
2023-01-20 19:10:49 +0100

    Add test 1_2

--- test_1/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116
-pip install imagen_pytorch==1.16.5
-python test_1.py
